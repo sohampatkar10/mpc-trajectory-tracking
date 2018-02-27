624cd08d96eefbc1094dda937f28e8640b683a89
soham <spatkar1@jhu.edu>
2018-02-27 15:44:19 -0500

    Tracking working after conversion to quad state

--- src/traj_tracking_sim.cpp
+++ src/traj_tracking_sim.cpp
@@ -10,6 +10,9 @@
 #include "acado_common.h"
 #include "acado_auxiliary_functions.h"
 
+#include <quad_dynamics.h>
+#include <flat_space_conversions.h>
+
 /* Some convenient definitions. */
 #define NX          ACADO_NX  /* Number of differential state variables.  */
 #define NXA         ACADO_NXA /* Number of algebraic variables. */
@@ -20,9 +23,10 @@
 #define NYN         ACADO_NYN /* Number of measurements/references on node N. */
 
 #define N           ACADO_N   /* Number of intervals in the horizon. */
-#define NUM_OBS   2
+#define NT        50
+#define NUM_OBS   1
 #define NUM_STEPS 10
-
+#define NP        3
 /* Global variables used by the solver. */
 ACADOvariables acadoVariables;
 ACADOworkspace acadoWorkspace;
@@ -46,177 +50,157 @@ int main(int argc, char** argv) {
   std::ifstream xref(reference_states);
   std::ifstream uref(reference_controls);
 
-  vector<double> ref_states;
-  vector<double> ref_controls;
+  vector<double> ref_states((NT+1)*NX);
+  vector<double> ref_controls(NT*NU);
 
   /* Initialize the solver. */
   acado_initializeSolver();
 
-  for (i = 0; i < N ; ++i) {
+  for (i = 0; i < NT ; ++i) {
+    std::string line;
+    std::getline(uref, line);
+    std::istringstream iss(line);
+    for(int u=0; u < NU; u++)
+      iss >> ref_controls[i*NU + u];
+  }
+
+  for (i = 0; i < NT+1 ; ++i) {
     std::string line;
     std::getline(xref, line);
     std::istringstream iss(line);
-    for(int x=0; x < NX; x++)
-      iss >> acadoVariables.y[i*NY + x];
-    std::getline(uref, line);
-    std::istringstream iss2(line);
-    for(int u=0; u < NU; u++)
-      iss2 >> acadoVariables.y[i*NY + NX + u];
+    for(int x=0; x < NX; x++) {
+      iss >> ref_states[i*NX + x];
+    }
   }
 
-  for (i = 0; i < N ; ++i) {
-    std::string line;
-    std::istringstream iss(line);
-    std::getline(uref, line);
-    for(int u=0; u < NU; u++)
-      iss >> acadoVariables.u[i*NU + u];
-  }
+  double l1 = 0.175; double l2 = 0.42;
 
-  for (i = 0; i < N+1 ; ++i) {
-    std::string line;
-    std::istringstream iss(line);
-    std::getline(xref, line);
-    for(int x=0; x < NX; x++)
-      iss >> acadoVariables.x[i*NX + x];
-  }
-
-  /* MPC: initialize the current state feedback. */
-#if ACADO_INITIAL_STATE_FIXED
   for (i = 0; i < NX; ++i) acadoVariables.x0[i] = 0.0;
   acadoVariables.x0[14] = -1.5;
-#endif
-  ROS_INFO("added init");
 
-  /* Prepare first step */
-  acado_preparationStep();
+  vector<double> quad_state(NX);
+  for(int i=0; i < NX; i++) quad_state[i] = 0.0;
+  quad_state[14] = -1.56;
+  tf::Transform quadTf = tf::Transform(
+                          tf::createQuaternionFromRPY(0,0,0),
+                          tf::Vector3(0,0,0));
+  QuadDynamics dynamicsPropogater(NP, 0.02, NX, NU, N);
+  FlatSpaceConversions flateSpaceConverter;
 
-  // /* Get the time before start of the loop. */
-  acado_tic(&t);
+  tf::TransformBroadcaster br;
+  ros::Publisher jointPub = nh.advertise<sensor_msgs::JointState>("/joint_states", 1);
 
-   /* The "real-time iterations" loop. */
-  for(iter = 0; iter < NUM_STEPS; ++iter) {
-    acado_feedbackStep();
-    acado_preparationStep();
+  int dt = 0;
+  double gx = 2.0; double gy = 0.0; double gz = 2.0;
+  ros::Publisher goalPub = nh.advertise<visualization_msgs::Marker>("/goal_marker", 1);
+  ros::Publisher obsPub = nh.advertise<visualization_msgs::Marker>("/obs_marker", 1);
+  ros::Publisher odomPub = nh.advertise<nav_msgs::Odometry>("/odometry", 1);
+
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = "world";
+  marker.id = 0;
+  marker.type = visualization_msgs::Marker::CUBE;
+  marker.action = visualization_msgs::Marker::ADD;
+  marker.pose.position.x = gx;
+  marker.pose.position.y = gy;
+  marker.pose.position.z = gz - 0.0875;
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+  marker.scale.x = 0.1;
+  marker.scale.y = 0.1;
+  marker.scale.z = 0.1;
+  marker.color.a = 1.0; 
+  marker.color.r = 0.0;
+  marker.color.g = 1.0;
+  marker.color.b = 0.0;
+
+  double obs[] = {1.0};
+  visualization_msgs::Marker obs_marker[NUM_OBS];
+
+  for(int o = 0; o < NUM_OBS; o++) {
+    obs_marker[o].header.frame_id = "world";
+    obs_marker[o].id = o;
+    obs_marker[o].type = visualization_msgs::Marker::SPHERE;
+    obs_marker[o].action = visualization_msgs::Marker::ADD;
+    obs_marker[o].pose.position.x = obs[o];
+    obs_marker[o].pose.position.y = 0.0;
+    obs_marker[o].pose.position.z = obs[o] + 0.2;
+    obs_marker[o].pose.orientation.x = 0.0;
+    obs_marker[o].pose.orientation.y = 0.0;
+    obs_marker[o].pose.orientation.z = 0.0;
+    obs_marker[o].pose.orientation.w = 1.0;
+    obs_marker[o].scale.x = 0.3;
+    obs_marker[o].scale.y = 0.3;
+    obs_marker[o].scale.z = 0.3;
+    obs_marker[o].color.a = 1.0; 
+    obs_marker[o].color.r = 1.0;
+    obs_marker[o].color.g = 0.0;
+    obs_marker[o].color.b = 0.0;
   }
-  /* Read the elapsed time. */
-  real_t te = acado_toc(&t);
 
-  printf("\n\n time:   %.3g seconds\n\n", te);
+  while(dt < (NT-N)) {
+    for(i=0; i < N; i++) {
+      for(int x=0; x < NX; x++) {
+        acadoVariables.y[i*NY + x] = ref_states[(i+dt)*NX + x];
+        acadoVariables.x[i*NX + x] = ref_states[(i+dt)*NX + x];
+      }
+      for(int u=0; u < NU; u++) {
+        acadoVariables.y[i*NY + NX + u] = ref_controls[(i+dt)*NU + u];
+        acadoVariables.u[i*NU + u] = ref_controls[(i+dt)*NU + u];
+      }
+    }
 
-  // ros::Duration(0.5).sleep();
-  // tf::TransformBroadcaster br;
-  // ros::Publisher jointPub = nh.advertise<sensor_msgs::JointState>("/joint_states", 1);
-  // ros::Publisher goalPub = nh.advertise<visualization_msgs::Marker>("/goal_marker", 1);
-  // ros::Publisher obsPub = nh.advertise<visualization_msgs::Marker>("/obs_marker", 1);
-  // ros::Publisher odomPub = nh.advertise<nav_msgs::Odometry>("/odometry", 1);
+    acado_preparationStep();
 
-  // visualization_msgs::Marker marker;
-  // marker.header.frame_id = "world";
-  // marker.id = 0;
-  // marker.type = visualization_msgs::Marker::CUBE;
-  // marker.action = visualization_msgs::Marker::ADD;
-  // marker.pose.position.x = gx + 0.42 + 0.175;
-  // marker.pose.position.y = gy;
-  // marker.pose.position.z = gz - 0.0875;
-  // marker.pose.orientation.x = 0.0;
-  // marker.pose.orientation.y = 0.0;
-  // marker.pose.orientation.z = 0.0;
-  // marker.pose.orientation.w = 1.0;
-  // marker.scale.x = 0.1;
-  // marker.scale.y = 0.1;
-  // marker.scale.z = 0.1;
-  // marker.color.a = 1.0; 
-  // marker.color.r = 0.0;
-  // marker.color.g = 1.0;
-  // marker.color.b = 0.0;
+    for(iter = 0; iter < NUM_STEPS; ++iter) {
+      acado_feedbackStep();
+      acado_preparationStep();
+    }
 
-  // // double obs[] = {1.5, 2.5};
-  // // visualization_msgs::Marker obs_marker[NUM_OBS];
+    for(int x=0; x < NX; x++) quad_state[x] = acadoVariables.x[NP*NX + x];
 
-  // // for(tnt o = 0; o < NUM_OBS; o++) {
-  // //   obs_marker[o].header.frame_id = "world";
-  // //   obs_marker[o].id = o;
-  // //   obs_marker[o].type = visualization_msgs::Marker::SPHERE;
-  // //   obs_marker[o].action = visualization_msgs::Marker::ADD;
-  // //   obs_marker[o].pose.position.x = obs[o];
-  // //   obs_marker[o].pose.position.y = obs[o] + 0.2;
-  // //   obs_marker[o].pose.position.z = obs[o] + 0.2;
-  // //   obs_marker[o].pose.orientation.x = 0.0;
-  // //   obs_marker[o].pose.orientation.y = 0.0;
-  // //   obs_marker[o].pose.orientation.z = 0.0;
-  // //   obs_marker[o].pose.orientation.w = 1.0;
-  // //   obs_marker[o].scale.x = 0.3;
-  // //   obs_marker[o].scale.y = 0.3;
-  // //   obs_marker[o].scale.z = 0.3;
-  // //   obs_marker[o].color.a = 1.0; 
-  // //   obs_marker[o].color.r = 1.0;
-  // //   obs_marker[o].color.g = 0.0;
-  // //   obs_marker[o].color.b = 0.0;
-  // // }
+    quadTf = flateSpaceConverter.flat2tf(quad_state);
 
-  for(int tt=0; tt < N+1; tt++) {
-  //   tf::Transform transform = tf::Transform(
-  //    tf::createQuaternionFromRPY(0,0,acadoVariables.x[NX*tt + 12]), 
-  //    tf::Vector3(acadoVariables.x[NX*tt],
-  //                acadoVariables.x[NX*tt + 1],
-  //                acadoVariables.x[NX*tt + 2]));
+    for(int s=0; s < NX; s++) states << quad_state[s] <<" ";
+    states <<"\n";
 
-  //   double x,y,z,ga,q1,q2;
-  //   x = acadoVariables.x[NX*tt];
-  //   y = acadoVariables.x[NX*tt + 1];
-  //   z = acadoVariables.x[NX*tt + 2];
-  //   ga = acadoVariables.x[NX*tt + 12];
-  //   q1 = acadoVariables.x[NX*tt + 14];
-  //   q2 = acadoVariables.x[NX*tt + 15];
-  //   double l1 = 0.175; double l2 = 0.42;
+    ros::Duration(0.5).sleep();
+    br.sendTransform(tf::StampedTransform(quadTf, ros::Time::now(), "world", "baselink"));
+    sensor_msgs::JointState joint_state;
+    joint_state.name.push_back("airbasetolink1");
+    joint_state.name.push_back("link1tolink2");
 
-      for(int s=0; s < NX; s++) states << acadoVariables.x[NX*tt + s] <<" ";
-      states <<"\n";
+    joint_state.position.push_back(-quad_state[14] + 1.57);
+    joint_state.position.push_back(-quad_state[15]);
+    joint_state.header.stamp = ros::Time::now();
 
-      for(int u=0; u < NU; u++) controls << acadoVariables.u[NU*tt + u] <<" ";
-      controls <<"\n";
+    jointPub.publish(joint_state);
+    goalPub.publish(marker);
+    obsPub.publish(obs_marker[0]);
 
-  //   ROS_INFO("ee : %f %f %f", x + (l1*cos(q1) + l2*cos(q1+q2))*cos(ga),
-  //     y + (l1*cos(q1) + l2*cos(q1+q2))*sin(ga),
-  //     z + l1*sin(q1) + l2*sin(q1+q2));
+    nav_msgs::Odometry odommsg;
 
-  //   ROS_INFO("joints: %f %f", q1, q2);
+    odommsg.header.stamp = ros::Time::now();
+    odommsg.header.frame_id = "world";
+    odommsg.pose.pose.position.x = quadTf.getOrigin().x();
+    odommsg.pose.pose.position.y = quadTf.getOrigin().y();
+    odommsg.pose.pose.position.z = quadTf.getOrigin().z();
 
-  //   sensor_msgs::JointState joint_state;
-  //   joint_state.name.push_back("airbasetolink1");
-  //   joint_state.name.push_back("link1tolink2");
+    odommsg.pose.pose.orientation.x = quadTf.getRotation().x();
+    odommsg.pose.pose.orientation.y = quadTf.getRotation().y();
+    odommsg.pose.pose.orientation.z = quadTf.getRotation().z();
+    odommsg.pose.pose.orientation.w = quadTf.getRotation().w();
 
-  //   joint_state.position.push_back(-acadoVariables.x[NX*tt + 14] + 1.57);
-  //   joint_state.position.push_back(-acadoVariables.x[NX*tt + 15]);
-  //   joint_state.header.stamp = ros::Time::now();
+    odommsg.twist.twist.linear.x = quad_state[3];
+    odommsg.twist.twist.linear.y = quad_state[4];
+    odommsg.twist.twist.linear.z = quad_state[5];
 
-  //   jointPub.publish(joint_state);
+    odomPub.publish(odommsg);
 
-  //   nav_msgs::Odometry odom;
-  //   odom.header.stamp = ros::Time::now();
-  //   odom.header.frame_id = "world";
-  //   odom.pose.pose.position.x = acadoVariables.x[NX*tt];
-  //   odom.pose.pose.position.y = acadoVariables.x[NX*tt+1];
-  //   odom.pose.pose.position.z = acadoVariables.x[NX*tt+2];
-
-  //   odom.pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0,0,acadoVariables.x[NX*tt + 12]);
-
-  //   odom.twist.twist.linear.x = acadoVariables.x[NX*tt + 3];
-  //   odom.twist.twist.linear.y = acadoVariables.x[NX*tt + 4];
-  //   odom.twist.twist.linear.z = acadoVariables.x[NX*tt + 5];
-
-  //   odom.twist.twist.angular.z = acadoVariables.x[NX*tt + 13];
-
-  //   odomPub.publish(odom);
-
-  //   br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "baselink"));
-  //   marker.header.stamp = ros::Time::now();
-  //   goalPub.publish(marker);
-  //   // for(tnt o = 0; o < NUM_OBS; o++) {
-  //   //   obs_marker[o].header.stamp = ros::Time::now();
-  //   //   obsPub.publish(obs_marker[o]);
-  //   // }
-  //   ros::Duration(1.0/N).sleep();
+    for(i = 0; i < NX; ++i) acadoVariables.x0[i] = acadoVariables.x[NP*NX + i];
+    dt += NP;
   }
   return 0;
 }
